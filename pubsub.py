0d1cb04f87a4994f72602f511d8e10d9c54b6a96
Juyotal <cesarkamga3@gmail.com>
2021-12-01 21:14:08 +0200

    wallets

--- pubsub.py
+++ pubsub.py
@@ -1,6 +1,8 @@
 from typing import List
 import time
 from backend.tests.blockchain.test_blockchain import blockchain
+from backend.wallet.transaction_pool import TransactionPool
+from backend.wallet.transactions import Transaction
 from pubnub.pubnub import PubNub
 from pubnub.pnconfiguration import PNConfiguration
 from pubnub.callbacks import SubscribeCallback
@@ -14,12 +16,14 @@ pnconfig.publish_key = 'pub-c-8295b3cf-d235-4ddd-a2f8-b7816095df6b'
 
 CHANNELS = {
     'TEST': 'TEST',
-    'BLOCK': 'BLOCK'
+    'BLOCK': 'BLOCK',
+    'TRANSACTION': 'TRANSACTION'
 }
 
 class Listener(SubscribeCallback):
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.blockchain = blockchain
+        self.transaction_pool = transaction_pool
 
     def message(self, pubnub, messaage_object):
         print(f'\n-- Channel: {messaage_object.channel} | Message: {messaage_object.message}')
@@ -34,6 +38,12 @@ class Listener(SubscribeCallback):
                 print('\n -- Succesfully replaced the local chain')
             except Exception as e:
                 print(f"\n -- Did not replace chain: {e}")
+        
+        elif messaage_object.channel == CHANNELS['TRANSACTION']:
+            transaction = Transaction.from_json(messaage_object.message)
+            self.transaction_pool.set_transaction(transaction)
+            print('\n -- set the new transaction in transaction pool')
+
 
 
 class PubSub():
@@ -42,10 +52,10 @@ class PubSub():
     Provides communication between the nodes of the blockchain network
     """
 
-    def __init__(self, blockchain):
+    def __init__(self, blockchain, transaction_pool):
         self.pubnub = PubNub(pnconfig)
         self.pubnub.subscribe().channels(CHANNELS.values()).execute()
-        self.pubnub.add_listener(Listener(blockchain))
+        self.pubnub.add_listener(Listener(blockchain, transaction_pool))
 
     def publish(self, channel, message):
         """
@@ -59,6 +69,11 @@ class PubSub():
         """
         self.publish(CHANNELS['BLOCK'], block.to_json())
 
+    def broadcast_transaction(self, transaction):
+        """
+        this will Broadcast a transaction to all nodes
+        """
+        self.publish(CHANNELS['TRANSACTION'], transaction.to_json())
 
 
 def main():
---
+++ wallet/wallet.py
@@ -0,0 +1,87 @@
+import json
+import uuid
+from backend.config import STARTING_BALANCE
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives.asymmetric import ec
+from cryptography.hazmat.primitives.asymmetric.utils import (
+    encode_dss_signature,
+    decode_dss_signature
+)
+from cryptography.hazmat.primitives import hashes, serialization
+from cryptography.exceptions import InvalidSignature
+from backend.config import STARTING_BALANCE
+
+class Wallet:
+    """
+    An individal wallet for a miner.
+    Keeps track of a miner's balance.
+    Allows a miner to authorize a transaction.
+    """
+
+    def __init__(self):
+        self.address = str(uuid.uuid4())[0:8]
+        self.balance = STARTING_BALANCE
+        self.private_key = ec.generate_private_key(
+            ec.SECP256K1(), 
+            default_backend
+        )
+        self.public_key = self.private_key.public_key()
+        self.serialize_public_key()
+
+    def sign(self, data):
+        """
+        Generate a signature based on the data using a local private key
+        """
+        return decode_dss_signature(self.private_key.sign(
+            json.dumps(data).encode('utf-8'), 
+            ec.ECDSA(hashes.SHA256())
+        ))
+
+
+    def serialize_public_key(self):
+        """
+        Reset the public key to its serialized version
+        """
+        self.public_key_bytes = self.public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+        self.public_key = self.public_key_bytes.decode('utf-8')
+
+   
+
+
+    @staticmethod
+    def verify(public_key, data, signature):
+        """
+        Verify a signature based on the original public key and data
+        """
+
+        deserialized_public_key = serialization.load_pem_public_key(
+            public_key.encode('utf-8'),
+            default_backend()
+        )
+
+        (r, s) = signature
+
+        try:
+            deserialized_public_key.verify(
+                encode_dss_signature(r,s),
+                json.dumps(data).encode('utf-8'),
+                ec.ECDSA(hashes.SHA256())
+        )
+            return True
+
+        except InvalidSignature:
+            return False
+
+def main():
+    wallet = Wallet()
+    print (f'wallet: {wallet.__dict__}')
+
+    data = { 'foo': 'bar' }
+    signature = wallet.sign(data)
+    print(signature)
+
+if __name__ == "__main__":
+    main()
